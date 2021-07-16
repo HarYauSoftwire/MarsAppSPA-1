8c121c2541e6c94bd776afaaa41bbe5bfb135f2b
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-16 16:12:19 +0100

    refactor SolInputFC

--- mars-app-spa/src/FCs/SolInputFC.tsx
+++ mars-app-spa/src/FCs/SolInputFC.tsx
@@ -7,39 +7,43 @@ export const SolInputFC: React.FC = () => {
     const { rover, manifest, setSolPhoto, sol, setSol } =
         useContext(selectedRoverContext);
     const maxSol: number = rover ? rover.max_sol : 0;
-
     return (
-        <>
-            {manifest && (
-                <PaddedDiv>
-                    <p>Martian days since landing:</p>
-                    <SolInputBox
-                        className="solInputBox"
-                        type="text"
-                        value={sol !== undefined ? sol.toString() : ""}
-                        onChange={(e) => {
-                            let newStr = e.target.value;
-                            const regexp = /^[0-9]+$/;
-                            if (newStr === "") {
-                                setSol(undefined);
-                                setSolPhoto(undefined);
-                            } else if (regexp.test(newStr)) {
-                                let sol = Number(newStr);
-                                if (sol > maxSol) {
-                                    sol = maxSol;
-                                    newStr = maxSol.toString();
-                                }
-                                setSol(sol);
-                                setSolPhoto(
-                                    manifest.photos.find(
-                                        (solPhoto) => solPhoto.sol === sol
-                                    )
-                                );
-                            }
-                        }}
-                    />
-                </PaddedDiv>
-            )}
-        </>
+        <PaddedDiv>
+            <p>Martian days since landing:</p>
+            <SolInputBox
+                className="solInputBox"
+                type="text"
+                value={sol !== undefined ? sol.toString() : ""}
+                onChange={(e) => {
+                    const solString = e.target.value;
+                    try {
+                        const sol = convertStringToSol(solString, maxSol);
+                        setSol(sol);
+                        setSolPhoto(
+                            manifest?.photos?.find(
+                                (solPhoto) => solPhoto.sol === sol
+                            )
+                        );
+                    } catch (error) {}
+                }}
+            />
+        </PaddedDiv>
     );
 };
+
+function convertStringToSol(solString: string, maxSol: number) {
+    const regexp = /^[0-9]+$/;
+    var sol: number | undefined;
+    if (solString === "") {
+        sol = undefined;
+    } else if (regexp.test(solString)) {
+        sol = Number(solString);
+        if (sol > maxSol) {
+            sol = maxSol;
+            solString = maxSol.toString();
+        }
+    } else {
+        throw Error("String is not a valid integer");
+    }
+    return sol;
+}
