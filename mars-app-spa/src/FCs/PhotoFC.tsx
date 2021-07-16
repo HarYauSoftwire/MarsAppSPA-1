35cedc7d6f63618bab19ab3b451b70035b9975ff
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-16 17:22:52 +0100

    add lightbox photo viewer

--- mars-app-spa/src/FCs/PhotoFC.tsx
+++ mars-app-spa/src/FCs/PhotoFC.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Component, useState } from "react";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 import { PhotoI } from "../nasaInterfaces";
 import { PhotoImage } from "../Styles/Photo";
 
 export const PhotoFC: React.FC<{ photo: PhotoI }> = ({ photo }) => {
+    const [visibility, setVisibility] = useState(false);
     return (
         <>
-            <PhotoImage className="marsPhoto" src={photo.img_src} />
+            <PhotoImage
+                className="marsPhoto"
+                src={photo.img_src}
+                onClick={() => setVisibility(true)}
+            />
+            {visibility && (
+                <Lightbox
+                    mainSrc={photo.img_src}
+                    onCloseRequest={() => setVisibility(false)}
+                />
+            )}
         </>
     );
 };
